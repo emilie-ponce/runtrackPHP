60c643c9b4467ad485dcec971dfd326e86d0e381
emilie-ponce <emilie.ponce@laplateforme.io>
2025-01-14 13:22:33 +0100

    j'ai du aussi refaire le job 3 dans lequel il fallait écrire quatre types de variables : boolean, entier, chaîne de caractères et nombre à virgyle flotante

---
+++ jour1/job03/index.php
@@ -0,0 +1,19 @@
+<?php
+/*variable type boolean*/
+    $isItWritten = true;
+    echo $isItWritten;
+    $isItWritten = false;
+    echo $isItWritten;
+
+/*variable type entier*/
+    $NombreEleve = 300;
+    echo $NombreEleve;
+
+/*variable type chaîne de caractères*/
+    $NomEcole = "LaPlateforme"
+    echo $NomEcole;
+
+/*variable nombre à virgule flotante*/
+    $prix = 300.00
+    echo $prix;
+?>
