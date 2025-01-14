ec24794ba49d33e1b24db7841f3ffabb2c2d6640
emilie-ponce <emilie.ponce@laplateforme.io>
2025-01-14 10:50:18 +0100

    j'ai terminé le job 3 dans lequel il fallait afficher les deux mêmes variables et commentez chacune de ces lignes d’une façon différente

---
+++ jour1/job02/index.php
@@ -0,0 +1,8 @@
+<?php
+    $str = "Hello LaPlateforme!"; //première variable//
+    echo $str;
+
+    /*même variable affiché une seconde fois*/
+    $str = "Hello LaPlateforme!";
+    echo $str;
+?>
