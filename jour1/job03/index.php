3777300cfc2b769278040ba9e6bfaf6fc1a128ab
emilie-ponce <emilie.ponce@laplateforme.io>
2025-01-14 12:15:29 +0100

    j'ai fait toutes les variables

--- jour1/job03/index.php
+++ jour1/job03/index.php
@@ -2,5 +2,20 @@
 
 /*variable type boolean*/
     $isWrittenLaPlateforme = true;
+    echo $isWrittenLaPlateforme;
+    $isWrittenLaPlateforme = false;
+    echo $isWrittenLaPlateforme;
+
+/*variable type entier*/
+    $NombreEntier = 300;
+    echo $NombreEntier;
+
+/*variable type chaîne de caractères*/
+    $NomDeLecole = "LaPlateforme";
+    echo $NomDeLecole;
+
+/*variable nombre à virgule flottante*/
+    $prix = 300.25;
+    echo $prix;
 
 ?>
