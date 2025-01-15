602548f1acbdc7efd9f9083b158398956a7a3a72
emilie-ponce <emilie.ponce@laplateforme.io>
2025-01-15 14:22:08 +0100

    job 3 terminé

--- jour3/job03/index.php
+++ jour3/job03/index.php
@@ -1,5 +1,10 @@
 <?php
     $str = "I'm sorry Dave I'm afraid I can't do that";
-    $voyelles = ["a", "e", "i", "o", "u"];
+    $voyelles = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
 
+        for ($i = 0; $i <= strlen($str); $i += 1){
+            if ($str[$i] == in_array($str[$i], $voyelles)){
+                echo $str[$i];
+            }
+        }   
 ?>
