cc58f65e4f649938a89439da6005db1a6a97daf3
emilie-ponce <emilie.ponce@laplateforme.io>
2025-01-15 14:37:39 +0100

    j'ai terminé le job 4

--- jour3/job04/index.php
+++ jour3/job04/index.php
@@ -1,9 +1,6 @@
 <?php
     $str = "Dans l'espace, personne ne vous entend crier";
-    $compteur = 0;
-
-    for ($i = 0; $i < strlen($str); $i++) {
-        $compteur++;
-    }
-    echo "Le nombre de caractères dans la chaîne est :" . $compteur;
+    $compteur = strlen($str);
+    echo "Le nombre de caractères dans la chaîne est:" . $compteur;
 ?>
+
