cfa5a9c25e2064a2b64f7f4b5f6a11c8bdde97b7
emilie-ponce <emilie.ponce@laplateforme.io>
2025-01-15 10:56:06 +0100

    j'ai terminé le job 1 en ajoutant la condition if et else pour tester si les nombres étaient pairs ou impairs

--- jour3/job01/index.php
+++ jour3/job01/index.php
@@ -1,4 +1,10 @@
 <?php
     $nombre = [200, 204, 173, 98, 171, 404, 459];
-
+    foreach ($nombre as $nombresDonnés) {
+        if ($nombresDonnés % 2 == 0) {
+            echo "$nombresDonnés est paire<br />";
+        } else {
+            echo "$nombresDonnés est impaire<br />";
+        }
+    }
 ?>
