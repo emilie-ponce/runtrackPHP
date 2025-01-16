8b65f919ff5278a8a8fbb63dcd40d56ead34c13c
emilie-ponce <emilie.ponce@laplateforme.io>
2025-01-16 11:05:14 +0100

    j'ai modifié la fonction strlen par la boucle while et fonction isset

--- jour3/job05/index.php
+++
@@ -1,4 +0,0 @@
-<?php
-    $str = "On n est pas le meilleur quand on le croit mais quand on le sait";
-    $dic = 
-?>
--- jour3/job06/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $str = "Les choses que l'on Possède finissent par nous posséder.";
-    $reversedStr = "";
-    for ($i = strlen($str) - 1; $i >= 0; $i--) {
-        $reversedStr .= $str[$i];
-    }
-    echo $reversedStr;
-?>
