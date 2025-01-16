516417bb5e1d8dcf023d252659a8d52728464763
emilie-ponce <emilie.ponce@laplateforme.io>
2025-01-16 10:47:10 +0100

    j'ai modifié le job 3 en remplaçant la fonction strlen par while et foreach

--- jour3/job03/index.php
+++ jour3/job03/index.php
@@ -1,10 +1,15 @@
 <?php
     $str = "I'm sorry Dave I'm afraid I can't do that";
     $voyelles = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
+    $i = 0;
 
-    for ($i = 0; $i < strlen($str); $i++) {
-        if (in_array($str[$i], $voyelles)) {
-            echo $str[$i];
+    while ($str[$i] != "") {
+        foreach ($voyelles as $voyelle) {
+            if ($str[$i] == $voyelle) {
+                echo $str[$i];
+                break; 
+            }
         }
-    }   
+        $i++; 
+    }
 ?>
