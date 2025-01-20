e63e584d7dcc4fd663a5cff847de2fddc85f4d2b
emilie-ponce <emilie.ponce@laplateforme.io>
2025-01-20 11:53:32 +0100

    j'ai placé mes fichers dans un dossier concernant le job en question car j'avais oublié

---
+++ jour6/job06/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulaire</title>
+</head>
+<body>
+    <form action="traitement.php" method="post">
+        <label for="style">Sélectionnez un style :</label>
+        <select name="style" id="style">
+            <option value="style1">Style1</option>
+            <option value="style2">Style2</option>
+            <option value="style3">Style3</option>
+        </select>
+        <button type="submit">Envoyer</button>
+    </form>
+</body>
+</html>
+
